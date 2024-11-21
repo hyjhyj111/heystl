b45cf29d616d238b41e56aa2f918878ff9b83bcc
hyjhyj111 <heyujie0711@outlook.com>
2024-11-21 20:33:21 +0800

    latest

--- src/unique_Ptr.cpp
+++ src/unique_Ptr.cpp
@@ -1,6 +1,7 @@
 #include "unique_Ptr.hpp"
-
+using namespace hey;
 int main() {
-
+    unique_ptr<int> q(new int);
+    
     return 0;
 }
--- src/unique_Ptr.hpp
+++ src/unique_Ptr.hpp
@@ -2,6 +2,23 @@
 #include "define.hpp"
 
 NAMESPACE_OPEN
-
+template<typename _Tp>
+struct Default_deleter{
+    void operator()(_Tp *p) {
+        delete p;
+    }
+};
+template<typename _Tp, typename Deleter = Default_deleter<_Tp>>
+class unique_ptr{
+private:
+    _Tp *p;
+    using deleter = Deleter;
+public:
+    explicit unique_ptr(_Tp *ptr) : p(ptr) {}
+    ~unique_ptr() {
+        if (p)
+            deleter(p);
+    }
+};
 
 NAMESPACE_CLOSE
